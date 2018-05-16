d5c5760f1930b2f4e76c5b8b759a60fcea757686
pamost <p.a.mostovoy@gmail.com>
2018-05-16 23:33:57 +0600

    db

--- server.go
+++ server.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"net/http"
-
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	_ "github.com/mattn/go-sqlite3"
+	"net/http"
 )
 
 func main() {
+
+	db := initDB("storage.db")
+	migrate(db)
+
 	// Echo instance
 	e := echo.New()
 
@@ -19,7 +24,7 @@ func main() {
 	// Recover from panics anywhere in the chain
 	e.Use(middleware.Recover())
 
-	// ORS gives web servers cross-domain access controls,
+	// CORS gives web servers cross-domain access controls,
 	// which enable secure cross-domain data transfers
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
@@ -28,17 +33,47 @@ func main() {
 
 	// Route => handler
 	e.GET("/", func(c echo.Context) error {
-
 		return c.JSON(http.StatusOK, "Hi!")
 	})
 
 	e.GET("/id/:id", func(c echo.Context) error {
 		requested_id := c.Param("id")
-		fmt.Println(requested_id);
+		fmt.Println(requested_id)
 		return c.JSON(http.StatusOK, requested_id)
 	})
 
 	e.GET("/get", func(c echo.Context) error { return c.JSON(200, "GET") })
 
 	e.Logger.Fatal(e.Start(":8000"))
+}
+
+func initDB(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+
+	// Here we check for any db errors then exit
+	if err != nil {
+		panic(err)
+	}
+
+	// If we don't get any errors but somehow still don't get a db connection
+	// we exit as well
+	if db == nil {
+		panic("db nil")
+	}
+	return db
+}
+
+func migrate(db *sql.DB) {
+	sql := `
+    CREATE TABLE IF NOT EXISTS todo (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR NOT NULL
+    );
+    `
+
+	_, err := db.Exec(sql)
+	// Exit if something goes wrong with our SQL statement above
+	if err != nil {
+		panic(err)
+	}
 }
